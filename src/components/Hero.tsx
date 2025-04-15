51d9eca5361bd61ff565f900ad00ca6b428bfd06
Muhammad Ragil Sahyuda <142285719+mragilsa@users.noreply.github.com>
2025-04-15 21:27:04 +0700

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ function Hero(){
         <div className="relative text-light">
             <div className="absolute top-0 left-0 w-full h-60 -mt-10 bg-[url('/src/assets/HeroBackgroundImage.png')] bg-cover bg-center bg-no-repeat rounded-2xl"></div>
             <div className="relative z-10 px-5 pt-8 flex flex-col gap-5">
-                <Profile greeting="Assalamu'alaikum" info="Start Your Al-Qur'an Journey" 
+                <Profile greeting="Bismillah" info="Start Your Al-Qur'an Journey" 
                 />
                 <Verse
                 verse="“He said: ‘Therein you shall live, and therein you shall die, and from it you shall be brought out (i.e. resurrected).’”"
